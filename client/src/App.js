caaaadd946c47eecd1c311e34684398f96fccce3
taha-m45160 <23100107@lums.edu.pk>
2022-04-10 03:47:40 +0500

    fend-courses

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Login from './pages/misc/Login/Login.js'
 import ChangePassword from './pages/admin/changePassword/changePassword';
 import CreateCourse from './pages/admin/createCourse/createCourse';
 import Enroll from './pages/admin/enroll/enroll'
-import StudentHome from './pages/student/home/home';
+import Courses from './pages/student/courses/courses'
 
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
               <Route path="/admin/changepassword" element={<ChangePassword />}/>
               <Route path="/admin/createcourse" element={<CreateCourse/>}/>
               <Route path="/admin/enroll" element={<Enroll/>}/>
-              <Route path="/student" element={<StudentHome/>}/>
+              <Route path="/student/courses" element={<Courses/>}/>
           </Routes>
       </Router>
     </div>
---
+++ client/src/pages/student/courses/courses.js
@@ -0,0 +1,53 @@
+import "./courses.css";
+import axios from "axios";
+import {Link, useNavigate} from 'react-router-dom';
+import Navbar from "../../../components/Navbar/Navbar";
+
+
+export default function Courses() {
+    const navigate = useNavigate();
+
+    const displayCourses = () => {
+        // try {
+        //     const enrolledCourses = await axios.get('http://localhost:8000/students/courses')
+        // } catch (err) {
+        //     console.log(err)
+        // }
+
+        const enrolledCourses = [
+            {code:'CS-300', title:'Advanced Programming'}, 
+            {code:'CS-582', title:'Distributed Systems'},
+            {code:'CS-535', title:'Machine Learning'},
+            {code:'CS-473', title:'Network Security'},
+            {code:'CS-310', title:'Algorithms'}
+        ]
+        const courseComponents = []
+
+        const toCourse = (ctitle) => {
+            navigate(`/student/courses/${ctitle}/home`)
+        }
+
+        enrolledCourses.map((course) => {
+            courseComponents.push(
+                <button className="tab" onClick={() => toCourse(course.code)}>
+                    <h3>{course.code}</h3>
+                    <h3>{course.title}</h3>
+                </button>
+            )
+        })
+
+        return courseComponents
+    }
+
+    return (
+        <div className="courses">
+            <Navbar/>
+
+            <div className="course-tabs">
+                {displayCourses().map((comp) => {
+                    return comp
+                })}
+            </div>
+        </div>
+    );
+}
