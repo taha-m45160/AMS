bd4915b941e02a331686d805429ba089dcff18aa
Bilal <23100091@lums.edu.pk>
2022-04-11 01:17:51 +0500

    db-Populate tweaks

--- database/Populate/populateTeaches.js
+++ database/Populate/populateTeaches.js
@@ -7,7 +7,8 @@ async function populateTeaches (){
     await createTeaches('mahira.khan', 2, 'CS-100', 'Fall', 2022)
     await createTeaches('mahira.khan', 2, 'CS-200', 'Fall', 2022)
     await createTeaches('hassan.raza', 1, 'CS-202', 'Spring', 2022)
-    await createTeaches('raheela.sharif', 1, 'CS-535', 'Fall', 2022)
+    await createTeaches('raheela.sharif', 1, 'CS-202', 'Spring', 2022)
+    await createTeaches('hassan.raza', 1, 'CS-535', 'Fall', 2022)
     await createTeaches('mursal.akhtar', 1, 'CS-368', 'Spring', 2022)
     await createTeaches('mursal.akhtar', 2, 'CS-368', 'Spring', 2022)
 }
