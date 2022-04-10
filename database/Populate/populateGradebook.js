5dc3586581d0de67282261c820cb3040001d5c0d
Mutahar <mutahar789@gmail.com>
2022-04-11 01:48:48 +0500

    db-entries-gradebook

--- database/Populate/populateGradebook.js
+++ database/Populate/populateGradebook.js
@@ -1,6 +1,19 @@
 const {createGradebook} = require('../Create/createGradebook')
 
 async function populateGradebook(){
+
+    await createGradebook('23100000', 1, 'CS-100', 'Fall', 2022, 'Quiz 1', 'A')
+    await createGradebook('23100000', 2, 'CS-200', 'Fall', 2022, 'Quiz 2', 'A-')
+    await createGradebook('23100000', 1, 'CS-202', 'Spring', 2022, 'Assignment 2', 'B')
+    await createGradebook('23100001', 2, 'CS-100', 'Fall', 2022, 'Quiz 2', 'A-')
+    await createGradebook('23100001', 1, 'CS-200', 'Fall', 2022, 'Quiz 1', 'A')
+    await createGradebook('23100001', 1, 'CS-202', 'Spring', 2022, 'Quiz 1', 'B+')
+    await createGradebook('23100002', 1, 'CS-100', 'Fall', 2022, 'Quiz 2', 'C+')
+    await createGradebook('23100002', 1, 'CS-200', 'Fall', 2022, 'Quiz 1', 'A-')
+    await createGradebook('23100002', 1, 'CS-202', 'Spring', 2022, 'Assignment 2', 'A')
+    await createGradebook('23100003', 2, 'CS-100', 'Fall', 2022, 'Assignment 2', 'C')
+    await createGradebook('23100003', 1, 'CS-202', 'Spring', 2022, 'Assignment 2', 'D')
+    await createGradebook('23100003', 1, 'CS-535', 'Fall', 2022, 'Quiz 2', 'A')
 }
 
 module.exports = {populateGradebook}
