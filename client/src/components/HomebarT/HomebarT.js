7958556e367c69666189c536d3d754f644b3c67e
sameearif88 <23100088@lums.edu.pk>
2022-04-10 14:33:30 +0500

    Teachers Homebar

---
+++ client/src/components/HomebarT/HomebarT.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './HomebarT.css'
+
+const Homebar = () => {
+    return (
+        <div className='bar'>
+            <ul className='bar-container'>
+                <li className='item'>
+                    <img src={require('../../images/Welcome.png')} width="50px"/>
+                    <a href="HOME-PAGE">Welcome</a>
+                </li>
+                <li className='item'>
+                <img src={require('../../images/Current Courses.png')} width="50px"/>
+                    <a href="CURRENT-COURSES">Current Courses</a>
+                </li>
+                <li className='item'>
+                <img src={require('../../images/Announcements.png')} width="50px"/>
+                    <a href="ANNOUNCEMENTS">Announcements</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Homebar
