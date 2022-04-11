781e05459700f32397d6511659704a13b8dc1f07
sameearif88 <23100088@lums.edu.pk>
2022-04-12 00:12:34 +0500

    Sidebar Teacher dataflow

--- client/src/components/Sidebar Teacher/SidebarT.js
+++ client/src/components/Sidebar Teacher/SidebarT.js
@@ -1,29 +1,140 @@
 import React from 'react'
 import './SidebarT.css'
+import {Link, useNavigate} from 'react-router-dom'
+import axios from 'axios';
 
 const SidebarT = () => {
+
+    const navigate = useNavigate();
+    const [errMsg, setErrMsg] = React.useState('')
+
+    const viewResources = async (ev) => {
+        ev.preventDefault();
+        try{
+            const res = await axios.get('http://localhost:8000/', {withCredentials: true});
+            if(res.data.resources.length === 0){
+                setErrMsg("No resources");
+            }
+            else{
+                navigate('/teacher/resources', {state: {
+                    resources: res.data.resources,
+                    userType: "teacher"
+                }})
+            }
+        }
+        catch(err){
+            if(err.response){
+                setErrMsg(err.response.statusText)
+            }
+        }
+    }
+
+    const viewQuizzes = async (ev) => {
+        ev.preventDefault();
+        try{
+            const res = await axios.get('http://localhost:8000/', {withCredentials: true});
+            if(res.data.quizzes.length === 0){
+                setErrMsg("No quiz");
+            }
+            else{
+                navigate('/teacher/quizzes', {state: {
+                    quizzes: res.data.quizzes,
+                    userType: "parent"
+                }})
+            }
+        }
+        catch(err){
+            if(err.response){
+                setErrMsg(err.response.statusText)
+            }
+        }
+    }
+
+    const viewAssignments = async (ev) => {
+        ev.preventDefault();
+        try{
+            const res = await axios.get('http://localhost:8000/', {withCredentials: true});
+            if(res.data.assignments.length === 0){
+                setErrMsg("No assignments");
+            }
+            else{
+                navigate('/teacher/assignments', {state: {
+                    assignments: res.data.assignments,
+                    userType: "teacher"
+                }})
+            }
+        }
+        catch(err){
+            if(err.response){
+                setErrMsg(err.response.statusText)
+            }
+        }
+    }
+
+    const viewGradebook = async (ev) => {
+        ev.preventDefault();
+        try{
+            const res = await axios.get('http://localhost:8000/', {withCredentials: true});
+            if(res.data.gradebook.length === 0){
+                setErrMsg("No gradebook");
+            }
+            else{
+                navigate('/teacher/gradebook', {state: {
+                    gradebook: res.data.gradebook,
+                    userType: "teacher"
+                }})
+            }
+        }
+        catch(err){
+            if(err.response){
+                setErrMsg(err.response.statusText)
+            }
+        }
+    }
+
+    const viewFeedback = async (ev) => {
+        ev.preventDefault();
+        try{
+            const res = await axios.get('http://localhost:8000/', {withCredentials: true});
+            if(res.data.feedback.length === 0){
+                setErrMsg("No feedback");
+            }
+            else{
+                navigate('/teacher/feedback', {state: {
+                    feedback: res.data.feedback,
+                    userType: "teacher"
+                }})
+            }
+        }
+        catch(err){
+            if(err.response){
+                setErrMsg(err.response.statusText)
+            }
+        }
+    }
+
     return (
         <div className='bar'>
             <ul className='bar-container'>
                 <li className='item'>
                     <img src={require('../../images/Resources.png')} width="50px"/>
-                    <a href="RESOURCES">Resources</a>
+                    <button className='click-items' onClick={viewResources}>Resources</button>
                 </li>
                 <li className='item'>
                 <img src={require('../../images/Quizzes.png')} width="50px"/>
-                    <a href="QUIZZES">Quizzes</a>
+                    <button  className='click-items' onClick={viewQuizzes}>Quizzes</button>
                 </li>
                 <li className='item'>
                 <img src={require('../../images/Assignments.png')} width="50px"/>
-                    <a href="ASSIGNMENTS">Assignments</a>
+                    <button className='click-items' onClick={viewAssignments}>Assignments</button>
                 </li>
                 <li className='item'>
                 <img src={require('../../images/Gradebook.png')} width="50px"/>
-                    <a href="GRADEBOOK">Gradebook</a>
+                    <button className='click-items' onClick={viewGradebook}>Gradebook</button>
                 </li>
                 <li className='item'>
                 <img src={require('../../images/Feedback.png')} width="50px"/>
-                    <a href="FEEDBACK">Feedback</a>
+                    <button className='click-items' onClick={viewFeedback}>Feedback</button>
                 </li>
             </ul>
         </div>
--- client/src/pages/admin/Homepage/HomepageA.js
+++ client/src/pages/admin/Homepage/HomepageA.js
@@ -2,12 +2,15 @@ import React from 'react'
 import './HomepageA.css'
 import Navbar from '../../../components/Navbar/Navbar'
 import Homebar from '../../../components/HomebarA/HomebarA'
+import Sidebar from '../../../components/Sidebar Teacher/SidebarT'
+import SidebarT from '../../../components/Sidebar Teacher/SidebarT'
 
 const HomepageA = () => {
     return (
         <div className='home-container'>
             <Navbar></Navbar>
-            <Homebar></Homebar>
+            {/* <Homebar></Homebar> */}
+            <SidebarT></SidebarT>
             <div className='text'>
                 <h2>Welcome to AMS! Where managing your academics just got a lot easier!</h2>
             </div>
