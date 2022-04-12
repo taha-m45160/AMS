25f8035b46c3a5f22a95d78a546674876c8d6200
Mutahar <mutahar789@gmail.com>
2022-04-12 16:18:15 +0500

    bend-mongoDB sanitization

--- server/rest_apis/admin/changePassword.js
+++ server/rest_apis/admin/changePassword.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose")
+const sanitize = require("mongo-sanitize")
 
 async function changePassword(req, res){
     try{
-        await mongoose.connection.db.collection('users').updateOne({ID: req.body.ID}, {$set: {password: req.body.password}})
+        const ID = sanitize(req.body.ID)
+        const password = sanitize(req.body.password)
+        await mongoose.connection.db.collection('users').updateOne({ID: ID}, {$set: {password: password}})
         res.status(200).send('Password successfully updated.')
     } catch(err) {
         res.status(400).send(err)
